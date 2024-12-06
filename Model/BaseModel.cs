008f9f21913d199e42ba40894dbe618ebc005966
Student <ratulkhan22555@gmail.com>
2024-12-06 18:23:58 +0600

    10

---
+++ Model/BaseModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Database.Model
+{
+    public class BaseModel
+    {
+        public DateTime ExamDate { get; set; } = DateTime.Now;
+        public string? CreatedBy { get; set; }
+        public DateTime UpdatedDate { get; set; } = DateTime.Now;
+        public string? UpdatedBy { get; set; }
+    }
+}
---
+++ Model/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Database.Model
+{
+    public class User : BaseModel
+    {
+        [Key]
+        public string UserId { get; set; } = Guid.NewGuid().ToString();
+        [Required]
+        public string? Name { get; set; }
+        [Required]
+        public string? Email { get; set; }
+        [Required]
+        public string? PasswordHash { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
